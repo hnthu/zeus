809d0e64a4b130e290ea3c7c09c20d4994d718bd
hnthu <huynhngocthu13@gmail.com>
2017-10-06 16:21:51 +0700

    golang project

---
+++ GoglandProjects/src/vote.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	_ "github.com/go-sql-driver/mysql"
+	"database/sql"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome, %s!", r.URL.Path[1:])
+}
+
+func main1() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8089", nil)
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:admin@tcp(192.168.99.100:32768)/mysql")
+	if err != nil {
+		panic(err.Error())  // Just for example purpose. You should use proper error handling instead of panic
+	}
+	defer db.Close()
+
+	// Prepare statement for inserting data
+	stmtIns, err := db.Prepare("INSERT INTO squareNum VALUES( ?, ? )") // ? = placeholder
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	defer stmtIns.Close() // Close the statement when we leave main() / the program terminates
+
+	// Prepare statement for reading data
+	stmtOut, err := db.Prepare("SELECT squareNumber FROM squareNum WHERE number = ?")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	defer stmtOut.Close()
+
+	// Insert square numbers for 0-24 in the database
+	for i := 0; i < 25; i++ {
+		_, err = stmtIns.Exec(i, (i * i)) // Insert tuples (i, i^2)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+	}
+
+	var squareNum int // we "scan" the result in here
+
+	// Query the square-number of 13
+	err = stmtOut.QueryRow(13).Scan(&squareNum) // WHERE number = 13
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	fmt.Printf("The square number of 13 is: %d", squareNum)
+
+	// Query another number.. 1 maybe?
+	err = stmtOut.QueryRow(1).Scan(&squareNum) // WHERE number = 1
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	fmt.Printf("The square number of 1 is: %d", squareNum)
+}
