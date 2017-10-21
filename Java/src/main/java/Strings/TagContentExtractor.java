db924bc0215f46480c6b07c4332ea9647abe58f6
hnthu <huynhngocthu13@gmail.com>
2017-10-21 11:12:29 +0700

    updated java project

---
+++ Java/src/main/java/Strings/TagContentExtractor.java
@@ -0,0 +1,33 @@
+package Strings;
+
+import java.util.Scanner;
+
+/*Sample Input
+
+4
+<h1>Nayeem loves counseling</h1>
+<h1><h1>Sanjay has no watch</h1></h1><par>So wait for a while</par>
+<Amee>safat codes like a ninja</amee>
+<SA premium>Imtiaz has a secret crush</SA premium>
+Sample Output
+
+Nayeem loves counseling
+Sanjay has no watch
+So wait for a while
+None
+Imtiaz has a secret crush
+*/
+public class TagContentExtractor {
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases>0){
+            String line = in.nextLine();
+
+            //Write your code here
+
+            testCases--;
+        }
+    }
+}
